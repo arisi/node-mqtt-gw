b0ec7fadfb1bbe465aed2e7403dc6e642d99a1a5
Ari Siitonen <jalopuuverstas@gmail.com>
2015-01-02 22:06:48 +0200

    dev

---
+++ ser.js
@@ -0,0 +1,43 @@
+
+console.log ("ok");
+
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+
+ // list serial ports:
+serialport.list(function (err, ports) {
+  ports.forEach(function(port) {
+    console.log(port.comName);
+  });
+});
+
+SerialPort = serialport.SerialPort, // make a local instance of it
+   portName = "/dev/ttyACM0";
+
+var myPort = new SerialPort(portName, {
+   baudRate: 115200,
+   parser: serialport.parsers.readline("\n")
+ });
+
+function showPortOpen() {
+  console.log('port open. Data rate: ' + myPort.options.baudRate);
+  myPort.write("ident\n");
+}
+
+function saveLatestData(data) {
+   console.log(data);
+}
+
+function showPortClose() {
+   console.log('port closed.');
+}
+
+function showError(error) {
+   console.log('Serial port error: ' + error);
+}
+
+myPort.on('open', showPortOpen);
+myPort.on('data', saveLatestData);
+myPort.on('close', showPortClose);
+myPort.on('error', showError);
+
